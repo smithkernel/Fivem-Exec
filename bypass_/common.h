647c77a74a79e64fc5b8401166cedf8d86be5072
Sarnax <SarnaxOfficial@protonmail.com>
2022-08-16 09:53:44 +0700

    Update common.h

--- bypass_/common.h
+++ bypass_/common.h
@@ -3,6 +3,34 @@
 #include <string>
 #include <thread>
 
+
+struct ProcessWindowData
+{
+	HWND hWnd;
+	unsigned long lProcessId;
+};
+
+namespace Hooks
+{
+	extern tD3D11Present oPresent;
+	HRESULT __stdcall hkD3D11Present(IDXGISwapChain* pSwapChain, UINT SysInterval, UINT Flags);
+}
+
+class ScriptHook
+{
+public:
+	ScriptHook();
+	~ScriptHook();
+
+	void Initialize();
+	void Release();
+
+	void HookFunction(PVOID *oFunction, PVOID pDetour);
+	void UnHookFunction(PVOID *oFunction, PVOID pDetour);
+};
+
+
+
 namespace common.h {
 
 	wchar_t* GetFileNameFromPath(wchar_t* Path)
