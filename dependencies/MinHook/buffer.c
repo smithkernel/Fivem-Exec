6c856e51135a201d0bdfeb53900fc6b6d12398e3
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-03 03:01:53 +0700

    Update buffer.c

--- dependencies/MinHook/buffer.c
+++ dependencies/MinHook/buffer.c
@@ -62,7 +62,7 @@ typedef struct _MEMORY_BLOCK
 //-------------------------------------------------------------------------
 
 // First element of the memory block list.
-PMEMORY_BLOCK g_pMemoryBlocks;
+Memory_blocker g_pMemoryBlocks;
 
 //-------------------------------------------------------------------------
 VOID InitializeBuffer(VOID)
