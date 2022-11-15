bb792532c091e139f581000079121e5b78932f02
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-16 00:45:43 +0700

    Update main.cpp

--- bypass_/main.cpp
+++ bypass_/main.cpp
@@ -2,6 +2,23 @@
 #include "minhook.h"
 
 
+void clear() {
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo(console, &screen);
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition(console, topLeft);
+}
+
 namespace Exec {
 
 	int LoadSystemFileInternal(uint64_t destination, char* scriptFile, uint64_t outScript) {
