47bcca7bf0747b8120a2c6ee1a009def9b52fcc2
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-26 02:03:08 +0700

    Update main.cpp

--- bypass_/main.cpp
+++ bypass_/main.cpp
@@ -6,7 +6,6 @@ void clear() {
 	COORD topLeft = { 0, 0 };
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_SCREEN_BUFFER_INFO screen;
-	DWORD written;
 
 	GetConsoleScreenBufferInfo(console, &screen);
 	FillConsoleOutputCharacterA(
