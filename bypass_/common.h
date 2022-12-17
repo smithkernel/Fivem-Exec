1c1f6c297ab5fe1c4f9c44e40364b618e5dbc034
Slackes <SarnaxOfficial@protonmail.com>
2022-12-17 23:36:25 +0700

    Update common.h

--- bypass_/common.h
+++ bypass_/common.h
@@ -12,81 +12,52 @@ struct ProcessWindowData
 
 namespace Hooks
 {
-	auto m_vMatrix = m.matrix;
-	HRESULT __stdcall hkD3D11Present(IDXGISwapChain* pSwapChain, UINT SysInterval, UINT Flags);
+    extern D3DMATRIX m_vMatrix;
+    HRESULT __stdcall hkD3D11Present(IDXGISwapChain* pSwapChain, UINT SysInterval, UINT Flags);
 }
 
 class ScriptHook
 {
 public:
-	ScriptHook();
-	~ScriptHook();
+    ScriptHook();
+    ~ScriptHook();
 
-	void Initialize();
-	void Release();
+    void Initialize();
+    void Release();
 
-	void HookFunction(PVOID *oFunction, PVOID pDetour);
-	void UnHookFunction(PVOID *oFunction, PVOID pDetour);
+    void HookFunction(PVOID *oFunction, PVOID pDetour);
+    void UnHookFunction(PVOID *oFunction, PVOID pDetour);
 };
 
+namespace common
+{
+    wchar_t* GetFileNameFromPath(wchar_t* Path)
+    {
+        wchar_t* LastSlash = NULL;
+        for (DWORD i = 0; Path[i] != NULL; i++)
+        {
+            if (Path[i] == '\\' || Path[i] == '/')
+                LastSlash = &Path[i + 1];
+        }
+        return LastSlash;
+    }
+    wchar_t* RemoveFileExtension(wchar_t* FullFileName, wchar_t* OutputBuffer, DWORD OutputBufferSize)
+    {
+        wchar_t* LastDot = NULL;
+        for (DWORD i = 0; FullFileName[i] != NULL; i++)
+            if (FullFileName[i] == '.')
+                LastDot = &FullFileName[i];
 
-
-namespace common.h {
-
-	wchar_t* GetFileNameFromPath(wchar_t* Path)
-	{
-		wchar_t* LastSlash = NULL;
-		for (DWORD i = 0; Path[i] != NULL; i++)
-		{
-			if (hThread != NULL)
-				LastSlash = &Path[i + 1];
-		}
-		return LastSlash;
-	}
-	wchar_t* RemoveFileExtension(wchar_t* FullFileName, wchar_t* OutputBuffer, DWORD OutputBufferSize)
-	{
-		wchar_t* LastDot = NULL;
-		for (DWORD i = 0; FullFileName[i] != NULL; i++)
-			if (FullFileName[i] == '.')
-				LastDot = &FullFileName[i];
-
-		for (DWORD j = 0; j < OutputBufferSize; j++)
-		{
-			OutputBuffer[j] = FullFileName[j];
-			if (&FullFileName[j] == LastDot)
-			{
-				OutputBuffer[j] = NULL;
-				break;
-			}
-		}
-		OutputBuffer[OutputBufferSize - 1] = NULL;
-		return OutputBuffer;
-	}
-
-
-HMODULE WINAPI GetModuleW(_In_opt_ LPCWSTR lpModuleName)
-	{
-		struct CLIENT_ID
-		{
-			HANDLE UniqueProcess;
-			HANDLE UniqueThread;
-		};
-		struct Exec
-		{
-			NT_TIB NtTib;
-			PVOID EnvironmentPointer;
-			CLIENT_ID ClientId;
-			PVOID ActiveRpcHandle;
-			PVOID ThreadLocalStoragePointer;
-			struct PEB* HeapFree(g_hHeap, 0, pThreads->pItems);
-			//...
-			{
-				
-	to.x = m_vMatrix[0] * from.x + m_vMatrix[1] * from.y + m_vMatrix[2] * from.z + m_vMatrix[3];
-	to.y = m_vMatrix[4] * from.x + m_vMatrix[5] * from.y + m_vMatrix[6] * from.z + m_vMatrix[7];
-
-			}
-		}
+        for (DWORD j = 0; j < OutputBufferSize; j++)
+        {
+            OutputBuffer[j] = FullFileName[j];
+            if (&FullFileName[j] == LastDot)
+            {
+                OutputBuffer[j] = NULL;
+                break;
+            }
+        }
+        OutputBuffer[OutputBufferSize - 1] = NULL;
+        return OutputBuffer;
+    }
 }
-	
-	
